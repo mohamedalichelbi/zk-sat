6477dedfd29a36db1f8055bf4237abfe68aa892b
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2023-07-02 18:51:51 +0200

    Add splr solver code to guest

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,35 +1,63 @@
 // TODO: Update the name of the method loaded by the prover. E.g., if the method
 // is `multiply`, replace `METHOD_NAME_ELF` with `MULTIPLY_ELF` and replace
 // `METHOD_NAME_ID` with `MULTIPLY_ID`
-use methods::{MULTIPLY_ELF, MULTIPLY_ID};
+use methods::{CERTIFY_ELF, CERTIFY_ID};
 use risc0_zkvm::{
     serde::{from_slice, to_vec},
     Executor, ExecutorEnv, ExecutorEnvBuilder
 };
 use std::time::{Instant, Duration};
 use rand::Rng;
+use dimacs::{ parse_dimacs, Instance, Sign };
+
 
 fn main() {
-    let start_time = Instant::now();
+    let max_vars: u64 = ((std::i32::MAX as i64) - (std::i32::MIN as i64)).try_into().unwrap();
+    let prb_name = "p1";
+    
+    let prb_file = format!("./splr-in/{}.cnf", prb_name);
+    let file_content = std::fs::read_to_string(prb_file)
+        .expect(&format!("failed to read file content for problem {}", prb_name));
+    let prb_instance = parse_dimacs(&file_content)
+        .expect(&format!("failed to parse dimacs for problem {}", prb_name));
 
-    let mut rng = rand::thread_rng();
+    // extract clauses
+    let (num_vars, clauses) = match prb_instance {
+        Instance::Cnf { num_vars, clauses } => (num_vars, clauses),
+        _ => unimplemented!("dimacs parsed file is not CNF"),
+    };
 
-    let a: u8 = rng.gen();
-    let b: u8 = rng.gen();
+    println!("Problem {} has {:?} clauses", prb_name, &clauses.len());
+
+    // convert clauses from i64 to i32 (the latter is used by the splr solver)
+    if num_vars > max_vars { unimplemented!("Problem {} has too many variables", prb_name); }
+    let splr_prb: Vec<Vec<i32>> = clauses.iter()
+        .map(|clause| {
+            let splr_literal: Vec<i32> = clause.lits().iter()
+                .map(|literal| {
+                    let var_name: i32 = literal.var().0.try_into().unwrap();
+                    let final_var = match literal.sign() {
+                        Sign::Pos => var_name,
+                        Sign::Neg => -var_name,
+                    };
+                    final_var
+                })
+                .collect();
+            splr_literal
+        })
+        .collect();
     
     // First, we construct an executor environment
     let env = ExecutorEnv::builder()
-        .add_input(&to_vec(&a).unwrap())
-        .add_input(&to_vec(&b).unwrap())
+        .add_input(&to_vec(&splr_prb).unwrap())
         .build();
 
     // Next, we make an executor, loading the (renamed) ELF binary.
-    let mut exec = Executor::from_elf(env, MULTIPLY_ELF).unwrap();
+    let mut exec = Executor::from_elf(env, CERTIFY_ELF).unwrap();
 
     // Run the executor to produce a session.
     let session = exec.run().unwrap();
 
-    println!("Session creation duration {:?}", start_time.elapsed());
     let start_time_prover = Instant::now();
 
     // Prove the session to produce a receipt.
@@ -42,8 +70,7 @@ fn main() {
     let start_time_verifier = Instant::now();
 
     // verify your receipt
-    receipt.verify(MULTIPLY_ID).unwrap();
+    receipt.verify(CERTIFY_ID).unwrap();
 
     println!("Verifier duration {:?}", start_time_verifier.elapsed());
-    println!("Full duration {:?}", start_time.elapsed());
 }
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -4,14 +4,22 @@
 
 
 use risc0_zkvm::guest::env;
+use splr::Certificate;
+
+extern crate alloc;
+
+use alloc::vec::Vec;
 
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
-    let a: u8 = env::read();
-    let b: u8 = env::read();
+    let sat_problem: Vec<Vec<i32>> = env::read();
+    // solve
+    let result: &str = match Certificate::try_from(sat_problem) {
+        Ok(Certificate::SAT(_ans)) => "SATISFIABLE",
+        Ok(Certificate::UNSAT) => "UNSATISFIABLE",
+        Err(e) => "UNKNOWN",
+    };
 
-    let c = a * b;
-
-    env::commit(&c);
+    env::commit(&result);
 }
